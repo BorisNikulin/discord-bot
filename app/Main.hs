36d30b9d78a3ccb115b9fe5d3f2e662c19cc3c97
Boris Nikulin <NikulinBE@gmail.com>
2019-06-17 04:19:19 -0700

    Minimal running example

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,13 @@
 module Main where
 
-main :: IO ()
-main = putStrLn "Hello, World!"
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+import Discord
+
+main :: IO (Either RestCallException Message)
+main = do
+	dis <- loginRest . Auth =<< getBotToken
+	restCall dis $ CreateMessage 221469616789127168 "test post pls ignore"
+
+getBotToken :: IO T.Text
+getBotToken = T.strip <$> TIO.readFile "./secrets/bot-token"
